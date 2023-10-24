5f1634c4327fed44bd5f6fb673fddce65a330155
Rene Rodriguez <afuereno@gmail.com>
2023-10-24 12:52:23 -0700

    love

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 // connect mongoose to DB
 
 const mongoose = require('mongoose');
-
-let connectionString = `mongodb+srv://userOne:${process.env.MONGO_PASS}@cluster0.loes5ey.mongodb.net/Company?retryWrites=true&w=majority`
+let connectionString =
+`mongodb+srv://DreamPop:${process.env.MONGO_PASS}@cluster0.0hbkfzj.mongodb.net/?retryWrites=true&w=majority`
+console.log(connectionString);
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const helmet = require('helmet'); // adds a bunch of standard security to server
+// const helmet = require('helmet'); // adds a bunch of standard security to server
 const Book = require('./models/Book.js');
 require('dotenv').config();
 require('./config/db.js');
@@ -17,16 +17,63 @@ app.use(cors({
     origin: "*"
 }));
 app.use(morgan('dev'));
-app.use(helmet());
+// app.use(helmet());
 // END MIDDLEWARE //
 
 // START ROUTES //
 
 
 // find   - finds everything
+
+app.get('/books/search/:keyword', async (req, res) => {
+    const keyword = req.params.keyword;
+  
+    try {
+      const matchingBooks = await Book.find({ title: { $regex: keyword, $options: 'i' } });
+  
+      if (matchingBooks.length > 0) {
+        res.send(matchingBooks);
+      } else {
+        res.send('No matching books found.');
+      }
+    } catch (error) {
+      res.status(500).send('Error, sorry.');
+    }
+  });
+  
+
+
+
 // .find()
+
+app.get("/books/title/:keyword", async (req,res)=>{
+   
+    let response = await Book.find({title:req.params.keyword})
+    res.send(response)
+})
+
+
+
+
  
 // findById
+
+app.get("/books/:id", async (req, res) => {
+    const bookId = req.params.id;
+    
+    // Use a database query method, like Mongoose if you're using MongoDB
+    try {
+        const book = await Book.findById(bookId);
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+        res.json(book);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 
 // insertMany
 app.post('/books', async (req, res) => {
@@ -37,7 +84,32 @@ app.post('/books', async (req, res) => {
     res.send(dbResponse);
 })
 
-// findOne
+// findByIdAndUpdate
+app.put('/books/:id', async (req, res) => {
+    const id = req.params.id;
+    const { title, pages } = req.body;
+  
+    try {
+      const updatedBook = await Book.findByIdAndUpdate(id, { title, pages }, { new: true });
+  
+      if (updatedBook) {
+        res.send('Updated');
+      } else {
+        res.send('Not Found');
+      }
+    } catch (error) {
+      res.status(500).send('Error, sorry.');
+    }
+  });
+  //findbyIdAndDelete
+
+  app.delete("/books/" , async (req, res)=>{
+
+  })
+
+
+
+
 
 // END ROUTES //
 
